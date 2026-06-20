ae45c17f1af59ae4896048e60a1f9fa6988e3780
azharutama <azharutama837@gmail.com>
2026-06-21 02:12:58 +0700

    add setup migration

---
+++ database/factories/RekapFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Rekap;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Rekap>
+ */
+class RekapFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
